ac0f7ba2e032f69914893e8672f607408eab6b08
Victor Inojosa <vijoin@gmail.com>
2021-12-10 09:08:55 -0300

    Create basic ERC721 contract implementation and its deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const deploy = async () => {
+    const [deployer] = await ethers.getSigners();
+    
+    console.log("Deploying contract with the account:", deployer.address);
+
+    PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+    const deployed = await PlatziPunks.deploy();
+
+    console.log("Platzi Punks is deployed at:", deployed.address);
+};
+
+deploy().then(() => process.exit(0))
+.catch((error) => {
+    console.log(error);
+    process.exit(1);
+})
