bdffe2eb4d14ad37a05cb4667ac9f32c8b8c9595
Victor Inojosa <vijoin@gmail.com>
2021-12-15 10:04:01 -0300

    Implement safeMint with Counter and ERC721Enumerable capabilities
    * https://docs.openzeppelin.com/contracts/4.x/api/token/erc721#ERC721Enumerable
    * https://docs.openzeppelin.com/contracts/4.x/api/utils#Counters
    * https://docs.openzeppelin.com/contracts/4.x/wizard

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -3,7 +3,39 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PlatziPunks is ERC721 {
+contract PlatziPunks is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("PlatziPunks", "PLPKS") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by inherited Contracts
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        super.supportsInterface(interfaceId);
+    }
 }
